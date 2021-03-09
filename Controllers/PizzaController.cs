b8073ea24e3f3b85797526f10724736dcebfd02c
Niek Kuijken <nkuijken@vxcompany.com>
2021-03-09 17:11:01 +0100

    added CustomExceptionFilter added globally in Startup.cs

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DotNetFilters.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetException()
+        {
+            throw new Exception();
+            return Ok();
+        }
+
+    }
+}
---
+++ Filters/CustomExceptionFilter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DotNetFilters.Filters
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+
+        public void OnException(ExceptionContext context)
+        {
+            var result = new BadRequestObjectResult("Custom error message.");
+            context.Result = result;
+        }
+    }
+}
